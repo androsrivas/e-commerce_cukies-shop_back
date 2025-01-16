e04f6240e7ef78424c214100b0cfc846ce8402ce
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-01-16 12:02:31 +0100

    dependency lombok added

---
+++ src/main/java/com/factoriaF5/cukies/CukiesApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaF5.cukies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CukiesApplication.class, args);
+	}
+
+}
