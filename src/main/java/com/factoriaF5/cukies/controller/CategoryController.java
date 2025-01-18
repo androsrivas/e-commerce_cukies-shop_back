60717b152015312568b3576c19e67c399065490f
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-01-18 16:45:55 +0100

    Read categories functionality added

--- src/main/java/com/factoriaF5/cukies/controller/CategoryController.java
+++ src/main/java/com/factoriaF5/cukies/controller/CategoryController.java
@@ -1,17 +1,13 @@
 package com.factoriaF5.cukies.controller;
 
 import com.factoriaF5.cukies.DTOs.category.CategoryDTO;
-import com.factoriaF5.cukies.DTOs.category.CategoryMapper;
-import com.factoriaF5.cukies.model.Category;
-import com.factoriaF5.cukies.repository.CategoryRepository;
 import com.factoriaF5.cukies.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -24,7 +20,12 @@ public class CategoryController {
 
     @PostMapping
     public ResponseEntity<CategoryDTO> saveCategory(@Valid @RequestBody CategoryDTO categoryDTO){
-        CategoryDTO newCategoryDTO = categoryService.saveCategory(categoryDTO);
+        CategoryDTO newCategoryDTO = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(newCategoryDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getCategories(){
+        return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
     }
 }
--- src/main/java/com/factoriaF5/cukies/service/CategoryService.java
+++ src/main/java/com/factoriaF5/cukies/service/CategoryService.java
@@ -2,9 +2,12 @@ package com.factoriaF5.cukies.service;
 
 import com.factoriaF5.cukies.DTOs.category.CategoryDTO;
 import com.factoriaF5.cukies.DTOs.category.CategoryMapper;
+import com.factoriaF5.cukies.exception.EmptyException;
 import com.factoriaF5.cukies.model.Category;
 import com.factoriaF5.cukies.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -13,9 +16,15 @@ public class CategoryService {
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
-    public CategoryDTO saveCategory(CategoryDTO categoryDTO){
+    public CategoryDTO createCategory(CategoryDTO categoryDTO){
         Category newCategory = CategoryMapper.dtoToEntity(categoryDTO);
         Category savedCategory = categoryRepository.save(newCategory);
         return CategoryMapper.entityToDTO(savedCategory);
+    }
+    public List<CategoryDTO> getCategories(){
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()) throw new EmptyException();
+        List<CategoryDTO> listCategories = categories.stream().map(category -> CategoryMapper.entityToDTO(category)).toList();
+        return listCategories;
     }
 }
