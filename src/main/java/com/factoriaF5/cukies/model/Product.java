6e192806671a0f1a1e4a885f1ff05fa63aa4d398
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-01-20 12:18:30 +0100

    crud product

---
+++ src/main/java/com/factoriaF5/cukies/model/Product.java
@@ -0,0 +1,29 @@
+package com.factoriaF5.cukies.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor @NoArgsConstructor
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+    private double price;
+    private String imageUrl;
+    private boolean featured;
+
+    public Product(String name, double price, String imageUrl, boolean featured) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.featured = featured;
+    }
+
+
+}
